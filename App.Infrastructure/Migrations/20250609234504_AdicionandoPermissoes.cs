9c2902d9506cdf169ad968cac62d68a2c65e298d
Vinicius Dias <diasvinicius95@outlook.com>
2025-06-09 20:53:50 -0300

    Feat: corrigindo problema das migrations + Incluindo documentacao e licença

---
+++ App.Infrastructure/Migrations/20250609234504_AdicionandoPermissoes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoPermissoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "UserRole",
+                table: "UserInformations",
+                type: "TINYINT",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRole",
+                table: "UserInformations");
+        }
+    }
+}
